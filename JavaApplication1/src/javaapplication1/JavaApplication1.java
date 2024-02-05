e223a9ad0b0d1f0382b848b9b8c2c23824904bf7
960622 <960622@PITLAB2178812.senac.educ>
2024-02-05 09:59:25 -0300

    Exercicios

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -14,37 +14,42 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // EXERCÍCIO 1
+        
+        // Exercício 1
         System.out.println("Hello World!");
         
-        // EXERCÍCIO 2
-        // THIS IS A SINGLE-LINE COMMENT
+        // Exercício 2
+        //this is a single-line comment
+        /*this
+            is
+            a
+            multi-lone comment*/
         /* THIS IS A MULTI-LINE COMMENT */
         
-        // EXERCÍCIO 3
+        // Exercício 3
         String carName = "Volvo";
         System.out.println(carName);
         
-        // EXERCÍCIO 4
+        // Exercício 4
         int maxSpeed = 120;
         System.out.println(maxSpeed);
         
-        // EXERCÍCIO 5
+        // Exercício 5
         int x = 5;
         int y = 10;
         System.out.println(x + y);
         
-        // EXERCÍCIO 6
+        // Exercício 6
         int z = x + y;
         System.out.println(z);
         
-        // EXERCÍCIO 7
-         x = 5;
-         y = 6;
-         z = 50;
+        // Exercício 7
+        x = 5;
+        y = 6;
+        z = 50;
         System.out.println(x + y + z);
         
-        // EXERCÍCIO 8
+        // Exercício 8
         int myNum = 9;
         System.out.println(myNum);
         float myFloatnum = 8.99f;
@@ -56,7 +61,7 @@ public class JavaApplication1 {
         String myText = "Hello World!";
         System.out.println(myText);
         
-        // EXERCÍCIO 9
+        // Exercício 9
         // JAVA DATA TYPES
         
         // EXERCÍCIO 10
@@ -64,69 +69,69 @@ public class JavaApplication1 {
         int myInt = (int) myDouble;
         System.out.println(myInt);
         
-        // EXERCÍCIO 11
+        // Exercício 11
         System.out.println(10 * 5);
         
-        // EXERCÍCIO 12
+        // Exercício 12
         System.out.println(10 / 5);
         
-        // EXERCÍCIO 13
+        // Exercício 13
         x = 10;
         x++;
         System.out.println(x);
         
-        // EXERCÍCIO 14
+        // Exercício 14
         x = 10;
         x += 5;
         System.out.println(x);
         
-        // EXERCÍCIO 15
+        // Exercício 15
         String greeting = "Hello!";
         System.out.println(greeting);
         
-        // EXERCÍCIO 16
+        // Exercício 16
         String txt = "hello!";
         System.out.println(txt.toUpperCase());
         
-        // EXERCÍCIO 17
+        // Exercício 17
         String firstName = "John";
         String lastName = "Doe";
         System.out.println(firstName + ' ' + lastName);
         
-        // EXERCÍCIO 18
+        // Exercício 18
         firstName = "John";
         lastName = "Doe";
         System.out.println(firstName.concat(lastName));
         
-        // EXERCÍCIO 19
+        // Exercício 19
         txt = "Hello Everybody";
         System.out.println(txt.indexOf("e"));
         
-        // EXERCÍCIO 20
+        // Exercício 20
         x = 5;
         y = 10;
         System.out.println(Math.max(x,y));
         
-        // EXERCÍCIO 21
+        // Exercício 21
        x = 16;
        System.out.println(Math.sqrt(x));
        
-       // EXERCÍCIO 22
+       // Exercício 22
        double rand = Math.random() * 1;
        System.out.println(rand);
        
-       //EXERCÍCIO 23
+       //Exercício 23
        boolean isJavaFun = true;
        boolean isFishTasty = false;
        System.out.println(isJavaFun);
        System.out.println(isFishTasty);
        
-       //EXERCÍCIO 24
+       //Exercício 24
        x = 10;
        y = 9;
        System.out.println(x > y);
        
-       // EXERCÍCIO 25
+       // Exercício 25
        x = 50;
        y = 10;
        
@@ -134,7 +139,7 @@ public class JavaApplication1 {
         System.out.println("Hello World!");   
        }
        
-       // EXERCÍCIO 26
+       // Exercício 26
        x = 50;
        y = 50;
        
@@ -142,7 +147,7 @@ public class JavaApplication1 {
         System.out.println("Hello World!");   
        }
        
-       // EXERCÍCIO 27
+       // Exercício 27
        x = 50;
        y = 50;
        
@@ -152,7 +157,7 @@ public class JavaApplication1 {
            System.out.println("No!");
        }
        
-       // EXERCÍCIO 28
+       // Exercício 28
        x = 50;
        y = 50;
        
@@ -164,12 +169,12 @@ public class JavaApplication1 {
            System.out.println("3");
        }
        
-       // EXERCÍCIO 29
+       // Exercício 29
        int time = 20;
        String result = (time < 18) ? "Good day." : "Good evening.";
        System.out.println(result);
        
-       // EXERCÍCIO 30
+       // Exercício 30
        int day = 2;
        switch (day) {
            
@@ -182,7 +187,7 @@ public class JavaApplication1 {
            break;
        }
        
-       // EXERCÍCIO 31
+       // Exercício 31
        day = 4;
        switch (day) {
            
@@ -200,14 +205,14 @@ public class JavaApplication1 {
        
        System.out.println("Hello World!");
        
-       // EXERCÍCIO 32
+       // Exercício 32
        int i = 1;
        while (i < 6){
            System.out.println(i);
            i++;
        }
        
-       // EXERCÍOCIO 33
+       // Exercício 33
        i = 1;
        do {
            System.out.println(i);
@@ -215,18 +220,18 @@ public class JavaApplication1 {
        }
        while (i < 6);
        
-       // EXERCÍCIO 34
+       // Exercício 34
        for ( i = 0; i < 5; i++){
            System.out.println("Sim!");
        }
        
-       // EXERCÍCIO 35
+       // Exercício 35
        String[] cars = {"Volvo", "BMW", "Ford"};
        for (String c : cars){
            System.out.println(c);
        }
        
-       // EXERCÍCIO 36
+       // Exercício 36
        for (i = 0; i < 10; i++){
            if (i == 5){
                break;
@@ -234,7 +239,7 @@ public class JavaApplication1 {
            System.out.println(i);
        }
        
-       // EXERCÍCIO 37
+       // Exercício 37
        for (i = 0; i < 10; i++) {
            if (i == 4) {
                continue;
@@ -242,43 +247,43 @@ public class JavaApplication1 {
             System.out.println(i);
         }
     
-       // EXERCÍCIO 38
+       // Exercício 38
        //String[] cars = {"Volvo", "BMW", "Ford"};
        
-       // EXERCÍCIO 39
+       // Exercício 39
        System.out.println(cars[1]);
        
-       // EXERCÍCIO 40
+       // Exercício 40
        cars[0] = ("Opel");
        System.out.println(cars[0]);
        
-       // EXERCÍCIO 41
+       // Exercício 41
        //String[] cars = {"Volvo", "BMW", "Ford"};
        System.out.println(cars.length);
        
-       // EXERCÍCIO 42
+       // Exercício 42
        //String[] cars = {"Volvo", "BMW", "Ford"};
        for (String c : cars){
            System.out.println(c);
        }
        
-       // EXERCÍCIO 43
+       // Exercício 43
        int[][] myNumbers = {{1,2,3,4}, {5,6,7}};
       
-       /* EXERCÍCIO 44
+       /* Exercício 44
        RESPOSTA myMethod(); */
        
-       /* EXERCÍCIO 45
+       /* Exercício 45
        RESPOSTA myMethod();
                 myMethod();*/
        
-       /* EXERCÍCIO 46
+       /* Exercício 46
        RESPOSTA:
        static void myMethod(String fname) {
             System.out.println(fname + " Doe");
         } */
        
-       /* EXERCÍCIO 47
+       /* Exercício 47
        RESPOSTA:
        static int myMethod(int x) {
            return 5 + x;
@@ -287,20 +292,20 @@ public class JavaApplication1 {
        
     }
     
-    // EXERCÍCIO 48
+    // Exercício 48
        public class MyClass {
          int x =5;  
     
-    // EXERCÍCIO 49
+    // Exercício 49
     MyClass myObj = new MyClass();
     
-    // EXERCÍCIO 50
+    // Exercício 50
         //System.out.println(myObj.x);
     
-    // EXERCÍCIO 50
+    // Exercício 50
     // myObj.myMethod();
     
-    /* EXERCÍCIO 51
+    /* Exercício 51
     public class MyClass {
   int x;
     }
@@ -311,19 +316,18 @@ public class JavaApplication1 {
     System.out.println(myObj.x);
     */
     
-    /* EXERCÍCIO 52
+    /* Exercício 52
     final class MyClass
     */
     
-    /* EXERCÍCIO 53
+    /* Exercício 53
     import java.util.Scanner;
     */
     
-    /* EXERCÍCIO 54
+    /* Exercício 54
     class Car extends Vehicle;
     */
     
-    }
-     
-    }
-    
+    } 
+       
+}
